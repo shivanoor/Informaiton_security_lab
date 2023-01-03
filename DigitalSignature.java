564ff09ec21bd72bc6ed390e9d1b93b0342349eb
rakesh_45 <113299998+shivanoor@users.noreply.github.com>
2023-01-03 20:44:16 +0530

    Add files via upload

---
+++ DigitalSignature.java
@@ -0,0 +1,26 @@
+import java.security.*;
+import java.util.*;
+import java.util.Base64.*;
+
+public class DigitalSignature {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        KeyPairGenerator kg = KeyPairGenerator.getInstance("RSA"); // RSA
+        kg.initialize(1024);// intialize size of key
+        KeyPair kp = kg.genKeyPair();
+        PrivateKey prikey = kp.getPrivate();
+        PublicKey pubkey = kp.getPublic();
+        String message = "This is the message to be signed";
+        Signature sg = Signature.getInstance("MD5WithRSA"); // SHA256WithRSA
+        sg.initSign(prikey);
+        sg.update(message.getBytes());
+        byte[] signedMessage = sg.sign();
+        String encodeMessage = Base64.getEncoder().encodeToString(signedMessage);
+        System.out.println("Signed Message is : " + encodeMessage);
+        sg.initVerify(pubkey);
+        sg.update(message.getBytes());
+        boolean verified = sg.verify(signedMessage);
+        System.out.println("Verification:" + verified);
+
+    }
+}
